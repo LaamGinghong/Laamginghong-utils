2106b6a11ac60e35dde487a1330b1b6345e5fa7a
LaamGinghong <351390560@qq.com>
2020-04-01 15:43:38 +0800

    build: remove babel, add ts-loader, rebuild webpack, package.json

--- scripts/types/error-overlay-webpack-plugin.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'error-overlay-webpack-plugin' {
-  import { Plugin } from 'webpack'
-
-  class ErrorOverlayWebpackPlugin extends Plugin {}
-
-  export = ErrorOverlayWebpackPlugin
-}
--- scripts/webpack.config.ts
+++ scripts/webpack.config.ts
@@ -6,16 +6,12 @@ import WebpackBuildNotifierPlugin from 'webpack-build-notifier'
 import CaseSensitivePathsWebpackPlugin from 'case-sensitive-paths-webpack-plugin'
 import CircularDependencyPlugin from 'circular-dependency-plugin'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
-import HardSourceWebpackPlugin from 'hard-source-webpack-plugin'
 import TerserPlugin from 'terser-webpack-plugin'
 import SpeedMeasurePlugin from 'speed-measure-webpack-plugin'
-import ErrorOverlayWebpackPlugin from 'error-overlay-webpack-plugin'
 
 const config: Configuration = {
   mode: 'production',
   entry: resolve(__dirname, '../', 'index.ts'),
-  devtool: 'source-map',
   output: {
     filename: 'index.js',
     path: resolve(__dirname, '../', 'lib'),
@@ -32,10 +28,13 @@ const config: Configuration = {
   module: {
     rules: [
       {
-        test: /\.(ts|js)$/,
-        loader: 'babel-loader',
+        test: /\.ts$/,
+        loader: 'ts-loader',
         exclude: /node_modules/,
-        options: { cacheDirectory: true },
+        options: {
+          experimentalFileCaching: true,
+          configFile: resolve(__dirname, '../', 'tsconfig.json'),
+        },
       },
     ],
   },
@@ -54,16 +53,8 @@ const config: Configuration = {
       allowAsyncCycles: false,
       cwd: resolve(__dirname, '../'),
     }),
-    new ErrorOverlayWebpackPlugin(),
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [resolve(__dirname, '../', 'types')],
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      memoryLimit: 1024 * 4,
-      tsconfig: resolve(__dirname, 'tsconfig.json'),
-    }),
-    new HardSourceWebpackPlugin({
-      info: { mode: 'none', level: 'warn' },
     }),
     new TerserPlugin(),
   ],
